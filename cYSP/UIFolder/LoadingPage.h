1466e18aeb6a438c62f24972e9ddfdce0a5b0a8b
JUNYI HE <77230422+tsingyayin@users.noreply.github.com>
2022-03-17 20:51:21 +0800

    Add files via upload
    增加光敏性癫痫警告

--- cYSP/UIFolder/LoadingPage.h
+++ cYSP/UIFolder/LoadingPage.h
@@ -8,6 +8,73 @@
 #include "../global_value.h"
 #include <QTest>
 
+class CENotice :public QWidget
+{
+	Q_OBJECT
+signals:
+	void isClosed();
+public:
+	QFrame* CentralWidget;
+	QLabel* NoticeLabel;
+	QPushButton* YesButton;
+	QGridLayout* CurrentLayout;
+	CENotice(QWidget* parent = Q_NULLPTR) {
+		this->setFixedSize(800, 300);
+		this->setParent(parent);
+		this->setWindowFlag(Qt::FramelessWindowHint);
+		this->setAttribute(Qt::WA_TranslucentBackground);
+		this->setWindowModality(Qt::ApplicationModal);
+		this->setStyleSheet("QWidget{color:#000000;};");
+		CentralWidget = new QFrame(this);
+		CentralWidget->setStyleSheet("QFrame{background-color:#FFFFFF;border:0px solid #555555;border-radius:10px;}");
+		CentralWidget->setFixedSize(800, 300);
+		NoticeLabel = new QLabel(this);
+		NoticeLabel->setText("光 敏 性 癫 痫 警 告\n\n\
+有极少数人在观看某些视觉内容时会突然癫痫发作，即使这些人并无癫痫病史。\n\
+程序本体中可能含有令您感到不适的程序动画行为，包括但不限于闪烁，渐变等等。\n\
+由您创建、编写或打开、预览的任一项目中亦可能存在会导致您感到不适的程序运行定义。\n\
+\n\
+不适症状包括但不限于头晕目眩，四肢、面部发生抽搐，甚至是神志不清、暂时失去意识等。\n\
+若您在使用程序时感到不适，应当立即退出程序或直接远离屏幕。若症状未能得到缓解，您应当立即向医生或医师求助，并遵医嘱。\n\
+");
+		NoticeLabel->setAlignment(Qt::AlignCenter);
+		NoticeLabel->setWordWrap(TRUE);
+		YesButton = new QPushButton(this);
+		YesButton->setFixedHeight(30);
+		connect(YesButton, SIGNAL(clicked()), this, SLOT(isYes()));
+		YesButton->setText("确定");
+		CurrentLayout = new QGridLayout(this);
+		CurrentLayout->addWidget(NoticeLabel, 0, 0, 1, 5);
+		CurrentLayout->addWidget(YesButton, 1, 2, 1, 1);
+		this->setLayout(CurrentLayout);
+		YesButton->setStyleSheet("\
+				QPushButton\
+					{border:2px solid #1473E6;border-radius:10px;background-color:#FFFFFF;}\
+				QPushButton:hover\
+					{border:2px solid #1473E6;border-radius:10px;background-color:#EEEEEE;}\
+				QPushButton:pressed\
+					{border:2px solid #1473E6;border-radius:10px;background-color:#DDDDDD;}");
+	}
+public slots:
+
+	void isYes() {
+		this->close();
+	}
+	void isNo() {
+		this->close();
+	}
+	void closeEvent(QCloseEvent* event) {
+		this->hide();
+		this->repaint();
+		emit isClosed();
+		this->disconnect();
+		this->deleteLater();
+	}
+	void mousePressEvent(QMouseEvent* event) {
+
+	}
+};
+
 class LoadingPage :public QFrame
 {
 	Q_OBJECT
@@ -70,6 +137,12 @@ public slots:
 		NoticeLabel->setText(msg("Kernal_Text_IFL_Version").arg("IFL_20211227"));
 		NoticeLabel->repaint();
 		QTest::qSleep(500);
+		CENotice* notice = new CENotice();
+		notice->show();
+		connect(notice, SIGNAL(isClosed()), this, SLOT(checking__()));
+	}
+	void checking__() {
+		QTest::qSleep(100);
 		for (float i = 0; i <= 3.1415 / 2; i += 0.02) {
 			QTest::qSleep(1);
 			Effect->setOpacity(1 - qSin(i));
